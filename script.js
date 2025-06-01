7eb33aa1856605e8f535c6a9d936e6e96515aacf
kristina-soreide <kristina.soreide@hotmail.com>
2025-06-01 14:46:07 +0200

    lager en test

--- script.js
+++ script.js
@@ -1,6 +1,36 @@
 const boksEL = document.querySelector(".boks")
-        boksEL.addEventListener("click",makePink)
-        
-        function makePink(){
-            boksEL.classList.toggle("pinkclass")
-        }
+boksEL.addEventListener("click", makePink)
+
+function makePink() {
+    boksEL.classList.toggle("pinkclass")
+}
+
+
+/*js til test*/
+const spørsmål[
+    {
+        spørsmål: "Hvem er du?",
+        svar: [
+            { text: "Lærer", correct:false}
+            { text: "Lærer", correct:true}
+            { text: "Lærer", correct:false}
+            { text: "Lærer", correct:false}
+        ]
+    },
+    {
+        spørsmål: "Hvem er du?",
+        svar: [
+            { text: "Lærer", correct:false}
+            { text: "Lærer", correct:true}
+            { text: "Lærer", correct:false}
+            { text: "Lærer", correct:false}
+    },
+    {
+        spørsmål: "Hvem er du?",
+        svar: [
+            { text: "Lærer", correct:false}
+            { text: "Lærer", correct:true}
+            { text: "Lærer", correct:false}
+            { text: "Lærer", correct:false}
+    }
+];
